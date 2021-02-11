d54ebdd7bc1eb3dfc41bdf13cb8888966ee56c00
Kam1runetz <notsoserious2017@gmail.com>
2021-02-11 22:23:15 +0300

    coverage script modified

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -11,6 +11,7 @@ CMAKE_OPTS="$CMAKE_LINKER_OPTS $CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS"
 
 cmake -H. -B_builds $CMAKE_OPTS -DBUILD_COVERAGE=ON
 cmake --build _builds
-cmake --build _builds --target test
+cmake --build _builds --target stack_test
+cmake --build _builds --target list_test
 cmake --build _builds --target gcov
 gcovr -r  .

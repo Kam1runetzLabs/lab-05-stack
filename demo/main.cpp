2cafff33a84cdbdaf251d17cb7ca60560f192f44
Kam1runetz <notsoserious2017@gmail.com>
2021-02-11 22:04:00 +0300

    added copyright

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,5 @@
+// Copyright 2021 Kam1runetzLabs <notsoserious2017@gmail.com>
+
 #include <stack.hpp>
 
-int main() {
-
-}
+int main() {}
--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,3 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2021 Kam1runetzLabs <notsoserious2017@gmail.com>
 
 #include <stack.hpp>

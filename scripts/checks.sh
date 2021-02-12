769e306f57d16ef68ab04283a96aafefe8fd1fc2
Kam1runetz <notsoserious2017@gmail.com>
2021-02-12 09:35:30 +0300

    scripts fixes

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -10,20 +10,16 @@ export CTEST_OUTPUT_ON_FAILURE=true
 # address sanitizer
 #CMAKE_LINKER_OPTS="-DCMAKE_EXE_LINKER='-fuse-ld=gold'"
 CMAKE_CONFIG_OPTS="-DHUNTER_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug"
-# shellcheck disable=SC2089
 CMAKE_TOOLCHAIN_OPTS="-DCMAKE_TOOLCHAIN_FILE='$(pwd)/tools/polly/sanitize-address-cxx17-pic.cmake'"
 CMAKE_OPTS="$CMAKE_LINKER_OPTS $CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS"
-cmake -H. -B_builds/sanitize-address-cxx17 "$CMAKE_OPTS"
+cmake -H. -B_builds/sanitize-address-cxx17 $CMAKE_OPTS
 cmake --build _builds/sanitize-address-cxx17
 ./_builds/sanitize-address-cxx17/stack_tests
 ./_builds/sanitize-address-cxx17/list_tests
 # thread sanitizer
-# shellcheck disable=SC2089
 CMAKE_TOOLCHAIN_OPTS="-DCMAKE_TOOLCHAIN_FILE='$(pwd)/tools/polly/sanitize-thread-cxx17-pic.cmake'"
 CMAKE_OPTS="$CMAKE_LINKER_OPTS $CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS"
-# shellcheck disable=SC2086
-# shellcheck disable=SC2090
 cmake -H. -B_builds/sanitize-thread-cxx17 $CMAKE_OPTS
 cmake --build _builds/sanitize-thread-cxx17
-./_builds/sanitize-address-cxx17/stack_tests
-./_builds/sanitize-address-cxx17/list_tests
+./_builds/sanitize-thread-cxx17/stack_tests
+./_builds/sanitize-thread-cxx17/list_tests
